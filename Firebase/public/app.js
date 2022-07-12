86f4b08680c072cb9f457d4104a0b611b0316d71
Max Yedid <maxyedid1@gmail.com>
2022-07-12 12:32:01 -0400

    Initializing firebase application

---
+++ Firebase/public/app.js
@@ -0,0 +1,35 @@
+
+const auth = firebase.auth();
+
+auth.signOut();
+
+const whenSignedIn = document.getElementById('whenSignedIn');
+const whenSignedOut = document.getElementById("whenSignedOut");
+
+const signInBtn = document.getElementById('signInBtn');
+const signOutBtn = document.getElementById('signOutBtn');
+
+const userDetails = document.getElementById('userDetails');
+
+
+const provider = new firebase.auth.GoogleAuthProvider();
+
+signInBtn.onclick = () => auth.signInWithPopup(provider);
+
+signOutBtn.onclick = () => auth.signOut();
+
+
+auth.onAuthStateChanged(user => {
+    if (user) {
+        //singed in
+        whenSignedIn.hidden = false;
+        whenSignedOut.hidden = true;
+        userDetails.innerHTML = `<h3>Hello ${user.displayName}!</h3> <p>User Id: ${user.uid}`
+        console.log(user);
+    } else {
+        //not signed in
+        whenSignedIn.hidden = true;
+        whenSignedOut.hidden = false;
+        userDetails.innerHTML = ' ';
+    }
+});

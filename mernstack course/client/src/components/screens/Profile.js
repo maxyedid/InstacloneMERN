0b5322c1bc28624cf132d848ba1bb65cb3a47762
Max Yedid <maxyedid1@gmail.com>
2022-07-20 15:04:25 -0400

    Created profile page layout with image, name, posts, followers, and following

--- mernstack course/client/src/components/screens/Profile.js
+++ mernstack course/client/src/components/screens/Profile.js
@@ -2,7 +2,28 @@ import React from "react";
 
 const Profile = () => {
     return (
-        <h1>Profile</h1>
+        <div>
+            <div style = {{
+                display: "flex",
+                justifyContent: "space-around",
+                margin: "18px 0px",
+                borderBottom: "1px solid grey"
+            }}>
+                <div>
+                <img style = {{width: "160px", height: "160px", borderRadius: "80px"}}
+                src = "https://media-exp2.licdn.com/dms/image/D4D03AQEzEKjDCSbRlQ/profile-displayphoto-shrink_800_800/0/1643615041664?e=1663804800&v=beta&t=hLZek1BO6-Lv3X73-d204iwvpL1p7l7UbpW5ySQtbcY"
+                alt = ""
+                 /> </div>
+            <div>
+                <h4>Max Yedid</h4>
+                    <div style = {{display: "flex", justifyContent: "space-between", width: "108%"}}>
+                        <h6>40 posts</h6>
+                        <h6>40 followers</h6>
+                        <h6>40 following</h6>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 

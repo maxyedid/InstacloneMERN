eec40b427d0fb61ec89d723fa8c3399ad880856b
Max Yedid <maxyedid1@gmail.com>
2022-07-20 14:46:20 -0400

    Added the sign up page along with links on both login and sign up to see if the user has an account or not

--- mernstack course/client/src/components/screens/Login.js
+++ mernstack course/client/src/components/screens/Login.js
@@ -5,11 +5,14 @@ const Login = () => {
         <div className = "mycard">
             <div className="card auth-card input-field">
                 <h2>Instagram</h2>
-                <input type = "text" placeholder = "email" />
-                <input type = "text" placeholder = "password" />
+                <input type = "text" placeholder = "Email" />
+                <input type = "text" placeholder = "Password" />
                 <button className ="btn waves-effect waves-light #64b5f6 blue lighten-2" type="submit" name="action">
                     Login
                 </button>
+                <h5>
+                    <a href = "./signup">Don't have an account?</a>
+                </h5>
             </div>
         </div>
     )
--- mernstack course/client/src/components/screens/Signup.js
+++ mernstack course/client/src/components/screens/Signup.js
@@ -2,7 +2,20 @@ import React from "react";
 
 const Signup = () => {
     return (
-        <h1>Sign Up</h1>
+        <div className = "mycard">
+            <div className="card auth-card input-field">
+                <h2>Instagram</h2>
+                <input type = "text" placeholder = "Name" />
+                <input type = "text" placeholder = "Email" />
+                <input type = "text" placeholder = "Password" />
+                <button className ="btn waves-effect waves-light #64b5f6 blue lighten-2" type="submit" name="action">
+                    Sign Up
+                </button>
+                <h5>
+                    <a href = "./login">Already have an account?</a>
+                </h5>
+            </div>
+        </div>
     )
 }
 

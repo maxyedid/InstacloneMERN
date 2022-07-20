6405a0a55175a3565e352c17562e1dcacbc59c27
Max Yedid <maxyedid1@gmail.com>
2022-07-20 16:07:23 -0400

    Made home page layout for posts and added a couple dummy posts

--- mernstack course/client/src/components/screens/Home.js
+++ mernstack course/client/src/components/screens/Home.js
@@ -2,7 +2,64 @@ import React from "react";
 
 const Home = () => {
     return (
-        <h1>Home</h1>
+        <div className = "home">
+            <div className = "card home-card">
+                <h5>Masked Wolf</h5>
+                <div className = "card-image">
+                    <img src = "https://img.freepik.com/premium-photo/astronaut-outer-open-space-planet-earth-stars-provide-background-erforming-space-planet-earth-sunrise-sunset-our-home-iss-elements-this-image-furnished-by-nasa_150455-16829.jpg?w=2000"
+                     alt = "Astronaut"
+                    />
+                </div>
+                <div className = "card-content">
+                <i class="material-icons" style = {{color: "red"}}>favorite</i>
+                    <h6>title</h6>
+                    <p>I feel like an astronaut in the ocean</p>
+                    <input type = "text" placeholder = "Add a comment" />
+                </div>
+            </div>
+            <div className = "card home-card">
+                <h5>Masked Wolf</h5>
+                <div className = "card-image">
+                    <img src = "https://img.freepik.com/premium-photo/astronaut-outer-open-space-planet-earth-stars-provide-background-erforming-space-planet-earth-sunrise-sunset-our-home-iss-elements-this-image-furnished-by-nasa_150455-16829.jpg?w=2000"
+                     alt = "Astronaut"
+                    />
+                </div>
+                <div className = "card-content">
+                <i class="material-icons" style = {{color: "red"}}>favorite</i>
+                    <h6>title</h6>
+                    <p>I feel like an astronaut in the ocean</p>
+                    <input type = "text" placeholder = "Add a comment" />
+                </div>
+            </div>
+            <div className = "card home-card">
+                <h5>Masked Wolf</h5>
+                <div className = "card-image">
+                    <img src = "https://img.freepik.com/premium-photo/astronaut-outer-open-space-planet-earth-stars-provide-background-erforming-space-planet-earth-sunrise-sunset-our-home-iss-elements-this-image-furnished-by-nasa_150455-16829.jpg?w=2000"
+                     alt = "Astronaut"
+                    />
+                </div>
+                <div className = "card-content">
+                <i class="material-icons" style = {{color: "red"}}>favorite</i>
+                    <h6>title</h6>
+                    <p>I feel like an astronaut in the ocean</p>
+                    <input type = "text" placeholder = "Add a comment" />
+                </div>
+            </div>
+            <div className = "card home-card">
+                <h5>Masked Wolf</h5>
+                <div className = "card-image">
+                    <img src = "https://img.freepik.com/premium-photo/astronaut-outer-open-space-planet-earth-stars-provide-background-erforming-space-planet-earth-sunrise-sunset-our-home-iss-elements-this-image-furnished-by-nasa_150455-16829.jpg?w=2000"
+                     alt = "Astronaut"
+                    />
+                </div>
+                <div className = "card-content">
+                <i class="material-icons" style = {{color: "red"}}>favorite</i>
+                    <h6>title</h6>
+                    <p>I feel like an astronaut in the ocean</p>
+                    <input type = "text" placeholder = "Add a comment" />
+                </div>
+            </div>
+        </div>
     )
 }
 

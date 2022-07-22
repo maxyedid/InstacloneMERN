831dbfe094a548450b00e7c8b150e8729f91a367
Max Yedid <maxyedid1@gmail.com>
2022-07-22 15:18:16 -0400

    Fixed significant bug where liking and unliking posts or even commenting would get rid of commenter's names and also the name of the user who posted the post

--- mernstack course/server/routes/post.js
+++ mernstack course/server/routes/post.js
@@ -54,7 +54,7 @@ router.put('/like', requiredLogin, (req, res) => {
         $push:{likes:req.user._id}
     }, {
         new: true
-    }).exec((err, result) => {
+    }).populate("postedBy", "_id name").populate("comments.postedBy", "_id name").exec((err, result) => {
         if (err) {
             return res.status(422).json({error: err})
         }
@@ -67,7 +67,7 @@ router.put('/unlike', requiredLogin, (req, res) => {
         $pull:{likes:req.user._id}
     }, {
         new: true
-    }).exec((err, result) => {
+    }).populate("postedBy", "_id name").populate("comments.postedBy", "_id name").exec((err, result) => {
         if (err) {
             return res.status(422).json({error: err})
         }

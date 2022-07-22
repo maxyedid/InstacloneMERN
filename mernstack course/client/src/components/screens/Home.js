7b78bc4c2cd4f6fdd22e7be37ade0016be543256
Max Yedid <maxyedid1@gmail.com>
2022-07-21 22:19:34 -0400

    Added like/unlike feature to the home page screen

--- mernstack course/client/src/components/screens/Home.js
+++ mernstack course/client/src/components/screens/Home.js
@@ -1,17 +1,64 @@
-import React, {useState, useEffect} from "react";
+import React, {useState, useEffect, useContext} from "react";
+import {UserContext} from '../../App'
 
 const Home = () => {
     const [data, setData] = useState([])
+    const {state, dispatch} = useContext(UserContext)
     useEffect(() => {
         fetch('http://localhost:4000/allposts', {
             headers: {
                 Authorization: "Bearer " + localStorage.getItem("jwt")
             }
         }).then(res => res.json()).then(result => {
-            console.log(result)
             setData(result.posts)
         })
     }, [])
+
+    const likePost = (id) => {
+            fetch("http://localhost:4000/like", {
+                method: "put",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": "Bearer " + localStorage.getItem("jwt")
+                },
+                body: JSON.stringify({
+                    postId: id
+                })
+            }).then(res => res.json()).then(result => {
+                const newData = data.map(item => {
+                    if (item._id == result._id) {
+                        return result;
+                    }
+                    return item;
+                })
+                setData(newData)
+            }).catch(err => {
+                console.log(err)
+            })
+    }
+
+    const unlikePost = (id) => {
+        fetch("http://localhost:4000/unlike", {
+            method: "put",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": "Bearer " + localStorage.getItem("jwt")
+            },
+            body: JSON.stringify({
+                postId: id
+            })
+        }).then(res => res.json()).then(result => {
+            const newData = data.map(item => {
+                if (item._id == result._id) {
+                    return result;
+                }
+                return item;
+            })
+            setData(newData)
+        }).catch(err => {
+            console.log(err)
+        })
+}
 
     return (
         <div className = "home">
@@ -26,7 +73,10 @@ const Home = () => {
                                 />
                             </div>
                             <div className = "card-content">
-                            <i className ="material-icons" style = {{color: "red"}}>favorite</i>
+                            <i className ="material-icons"
+                            onClick = {item.likes.includes(state._id)?() => unlikePost(item._id): () => likePost(item._id)}
+                            style = {item.likes.includes(state._id)? {color:"red"}: {color: "black"}}>favorite</i>
+                                <h6>{item.likes.length} likes</h6>
                                 <h6>{item.title}</h6>
                                 <p>{item.body}</p>
                                 <input type = "text" placeholder = "Add a comment" />

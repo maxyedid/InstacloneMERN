5f98eea5e62ef4f0b24901e8320c490dc2b651cf
Max Yedid <maxyedid1@gmail.com>
2022-07-25 20:52:56 -0400

    Making configuration for keys for production for heroku deployment and production

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const app = express();
 const mongoose = require('mongoose')
-const {MONGOURI} = require('./keys')
+const {MONGOURI} = require('./config/keys')
 const cors = require('cors');
 
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 app.use(cors());
 
@@ -29,6 +29,14 @@ app.use(express.json())
 app.use(require('./routes/auth'))
 app.use(require('./routes/post'))
 app.use(require('./routes/user'))
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static('client/build'))
+    const path = require('path')
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
 
 app.listen(PORT, () => {
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose')
 const User = mongoose.model("User");
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
-const {JWT_SECRET} = require('../config/keys')
+const {JWT_SECRET} = require('../server/keys')
 const requireLogin = require('../middleware/requiredLogin');
 
 router.get('/', (req, res)=> {
